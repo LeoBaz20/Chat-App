ece20f92d33f1334b904d2771e7ca8a4b2fa96e2
LeoBaz20 <89750451+LeoBaz20@users.noreply.github.com>
2024-07-22 18:19:15 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const https = require('https');
+const http = require('http');
 const { setupWebSocket } = require('./websocket/websocket');
 const userRoutes = require('./routes/userRoutes');
 const messageRoutes = require('./routes/messageRoutes')
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 
-const server = https.createServer(app);
+const server = http.createServer(app);
 setupWebSocket(server);
 
 server.listen(port, () => {
